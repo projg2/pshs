d43433252ea5399fa6b7a769f905babb99fb8f43
Michał Górny <mgorny@gentoo.org>
2020-09-26 11:36:01 +0200

    rtnl: Use getifaddrs() to get local IP address
    Use getifaddrs() instead of unstable and complex libnetlink.  This fixes
    getting IP addresses on systems with recent iproute2 versions
    (and without it).
    
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- src/rtnl.cxx
+++ src/rtnl.cxx
@@ -1,5 +1,5 @@
-/* pshs -- netlink IP getting support
- * (c) 2011 Michał Górny
+/* pshs -- IP address getting support
+ * (c) 2011-2020 Michał Górny
  * 2-clause BSD-licensed
  */
 
@@ -9,95 +9,28 @@
 
 #include <stdlib.h>
 
-#ifdef HAVE_LIBNETLINK
-#	include <stdio.h>
-#	include <string.h>
-#	include <errno.h>
-
+#ifdef HAVE_GETIFADDRS
+#	include <sys/types.h>
 #	include <sys/socket.h>
-
-extern "C"
-{
-#		include <libnetlink.h>
-};
 
 #	include <netinet/in.h>
 #	include <netinet/ip.h>
-#	include <arpa/inet.h>
+
+#	include <ifaddrs.h>
+#	include <netdb.h>
+#	include <string.h>
 #endif
 
 #include "network.h"
 
-#ifdef HAVE_LIBNETLINK
+#if HAVE_GETIFADDRS
 enum is_local { /* most preferred first */
 	ISLOCAL_NO, /* global address */
 	ISLOCAL_NET, /* address reserved for local network */
 	ISLOCAL_APIPA, /* automatic address assignment */
-	ISLOCAL_HOST /* localhost address */
+	ISLOCAL_HOST, /* localhost address */
+	ISLOCAL_MAX,
 };
-
-struct addr_search_data
-{
-	const char* addr;
-	int scope;
-	enum is_local islocal;
-};
-
-/**
- * store_addr
- * @sa: netlink address
- * @n: message header
- * @data: output buffer
- *
- * Handle netlink address message. Put the most useful IP address found in
- * output struct @data.
- *
- * Returns: 0
- */
-static int store_addr(const struct sockaddr_nl* sa, struct nlmsghdr* n, void* data)
-{
-	struct addr_search_data* out = static_cast<addr_search_data*>(data);
-	struct ifaddrmsg* addr = static_cast<ifaddrmsg*>(NLMSG_DATA(n));
-	struct rtattr*  rta_tb[IFA_MAX+1];
-
-	/* Based heavily on iproute2,
-	 * IOW: I'm not sure that I want to know what happens here. */
-	parse_rtattr(rta_tb, IFA_MAX, IFA_RTA(addr),
-			n->nlmsg_len - NLMSG_LENGTH(sizeof(*addr)));
-
-	if (addr->ifa_family == AF_INET && rta_tb[IFA_LOCAL])
-	{
-		/* Get the actual IP address */
-		struct sockaddr_in* in = static_cast<sockaddr_in*>(
-				static_cast<void*>(rta_tb[IFA_LOCAL]));
-		/* using char[4] allows us to ignore endianness */
-		unsigned char* binaddr = static_cast<unsigned char*>(
-				static_cast<void*>(&(in->sin_addr.s_addr)));
-
-		enum is_local islocal = ISLOCAL_NO;
-
-		if (binaddr[0] == 127) /* localhost */
-			islocal = ISLOCAL_HOST;
-		else if (binaddr[0] == 10)
-			islocal = ISLOCAL_NET;
-		else if (binaddr[0] == 192 && binaddr[1] == 168)
-			islocal = ISLOCAL_NET;
-		else if (binaddr[0] == 169 && binaddr[1] == 254)
-			islocal = ISLOCAL_APIPA;
-		else if (binaddr[0] == 172 && binaddr[1] >= 16 && binaddr[1] < 32)
-			islocal = ISLOCAL_NET;
-
-		/* prefer global scope, and global addresses */
-		if (!out->addr || addr->ifa_scope < out->scope || islocal < out->islocal)
-		{
-			out->addr = inet_ntoa(in->sin_addr);
-			out->scope = addr->ifa_scope;
-			out->islocal = islocal;
-		}
-	}
-
-	return 0;
-}
 #endif
 
 /**
@@ -109,26 +42,76 @@ static int store_addr(const struct sockaddr_nl* sa, struct nlmsghdr* n, void* da
  */
 const char* get_rtnl_external_ip(void)
 {
-#ifdef HAVE_LIBNETLINK
-	struct rtnl_handle rth;
-	struct addr_search_data out = { NULL, 0, ISLOCAL_NO };
+	const char* out = NULL;
 
-	if (rtnl_open(&rth, 0) < 0)
+#ifdef HAVE_GETIFADDRS
+	struct ifaddrs* addrs;
+	if (getifaddrs(&addrs) < 0)
 	{
-		std::cerr << "rtnl_open() failed" << std::endl;
+		std::cerr << "getifaddrs() failed: " << strerror(errno)
+			<< std::endl;
 		return NULL;
 	}
 
-	if (rtnl_wilddump_request(&rth, AF_INET, RTM_GETADDR) >= 0)
-	{
-		if (rtnl_dump_filter(&rth, store_addr, &out) < 0)
-			std::cerr << "rtnl_dump_filter() failed" << std::endl;
-	} else
-		std::cerr << "rtnl_wilddump_request() failed: " << strerror(errno)
-			<< std::endl;
+	struct sockaddr* curr_addr = NULL;
+	enum is_local curr_islocal = ISLOCAL_MAX;
 
-	rtnl_close(&rth);
-	return out.addr;
+	for (struct ifaddrs* addr = addrs; addr; addr = addr->ifa_next)
+	{
+		if (addr->ifa_addr == NULL)
+			continue;
+
+		int family = addr->ifa_addr->sa_family;
+		if (family == AF_INET)
+		{
+			struct sockaddr_in* in = static_cast<sockaddr_in*>(
+					static_cast<void*>(addr->ifa_addr));
+			/* using char[4] allows us to ignore endianness */
+			unsigned char* binaddr = static_cast<unsigned char*>(
+					static_cast<void*>(&(in->sin_addr.s_addr)));
+
+			enum is_local islocal = ISLOCAL_NO;
+			if (binaddr[0] == 127) /* localhost */
+				islocal = ISLOCAL_HOST;
+			else if (binaddr[0] == 10)
+				islocal = ISLOCAL_NET;
+			else if (binaddr[0] == 192 && binaddr[1] == 168)
+				islocal = ISLOCAL_NET;
+			else if (binaddr[0] == 169 && binaddr[1] == 254)
+				islocal = ISLOCAL_APIPA;
+			else if (binaddr[0] == 172 && binaddr[1] >= 16 && binaddr[1] < 32)
+				islocal = ISLOCAL_NET;
+
+			/* prefer global addresses */
+			if (islocal < curr_islocal)
+			{
+				curr_addr = addr->ifa_addr;
+				curr_islocal = islocal;
+			}
+		}
+		else if (family == AF_INET6)
+		{
+			// TODO: IPv6
+		}
+	}
+
+	if (curr_addr)
+	{
+		static char host[NI_MAXHOST];
+		socklen_t addr_len =
+			curr_addr->sa_family == AF_INET ?
+			sizeof(struct sockaddr_in) :
+			sizeof(struct sockaddr_in6);
+		if (getnameinfo(curr_addr, addr_len, host, sizeof(host),
+				NULL, 0, NI_NUMERICHOST) == 0)
+			out = host;
+		else
+			std::cerr << "getnameinfo() failed: " << strerror(errno)
+				<< std::endl;
+	}
+
+	freeifaddrs(addrs);
 #endif
-	return NULL;
+
+	return out;
 }
